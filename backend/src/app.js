39f5b622ce6483234822ac9d04d80283cde0b03d
Jatinkirodiwal <jatinkirodiwal21@gmail.com>
2026-06-13 14:40:21 +0530

    ini

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
+  "https://shope-verse-1.onrender.com",
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
