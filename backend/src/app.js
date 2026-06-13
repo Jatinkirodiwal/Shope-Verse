3109d833f4a8b0a95eb76b3b1bfbb75b508f9095
Jatinkirodiwal <jatinkirodiwal21@gmail.com>
2026-06-13 07:52:16 +0530

    updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,6 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
-  "https://shop-verse-2.onrender.com",
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
